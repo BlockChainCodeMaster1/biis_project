5f6b35f4469087caf7d4e4211354dac8e4b23b96
aemoe <>
2024-01-08 18:44:04 +0800

    fixed

--- server/container/soga.js
+++ server/container/soga.js
@@ -2,31 +2,84 @@ import db from "../database/db.js";
 const SOGA = db.SOGA;
 
 const WHITELIST = [
-    "9ppiEsLybEazCHHaM1y2AcpEPgHX82aHPZD2E5LuL4Z1",
-    "9ppiEsLybEazCHHaM1y2AcpEPgHX82aHPZD2E5LuL4ZQ",
+  "9ppiEsLybEazCHHaM1y2AcpEPgHX82aHPZD2E5LuL4Z1",
+  "9ppiEsLybEazCHHaM1y2AcpEPgHX82aHPZD2E5LuL4ZQ",
 ];
 
 export async function getWhiteListAuth(req, res) {
-    const { address } = req.params;
+  const { address } = req.params;
 
-    const include_whitelist = WHITELIST.indexOf(address)
-    console.log("include_whitelist", include_whitelist);
+  const is_whitelist = WHITELIST.indexOf(address);
+  console.log("include_whitelist", include_whitelist);
 
-    const is_whitelist = await SOGA.count({
-        where: {
-        address: address,
-        },
-    });
+  const is_minted = await SOGA.count({
+    where: {
+      address: address,
+    },
+  });
 
-    res.send({
-        msg: "success",
-        code: 1,
-        data: {
-            minted: is_whitelist > 0 ? true : false,
-            whiteList: include_whitelist != -1 ? true : false
-        },
-    });
+  res.send({
+    msg: "success",
+    code: 1,
+    data: {
+      minted: is_minted > 0 ? true : false,
+      whiteList: is_whitelist != -1 ? true : false,
+    },
+  });
 
-    // console.log("is_whitelist", is_whitelist)
+  // console.log("is_whitelist", is_whitelist)
 }
 
+export async function whiteListMint(req, res) {
+  let { address, txid } = req.body;
+
+  if (!address || !txid) {
+    res.send({
+      msg: "Incomplete parameter",
+      code: 0,
+    });
+    return;
+  }
+
+  const mint_number = await SOGA.count();
+
+  if (mint_number >= 2) {
+    res.send({
+      msg: "Whitelist Mint is overflow",
+      code: 0,
+    });
+    return;
+  }
+
+  const is_minted = await SOGA.count({
+    where: {
+      address: address,
+    },
+  });
+
+  if (is_minted > 0) {
+    res.send({
+      msg: "Already mint",
+      code: 0,
+    });
+    return;
+  }
+
+  const result = await SOGA.create({
+    address: address,
+    tx: txid,
+    is_whitelist: "true",
+  });
+
+  if (result) {
+    res.send({
+      msg: "Success",
+      code: 1,
+    });
+  } else {
+    res.send({
+      msg: "Save failure",
+      code: 0,
+    });
+  }
+}
--- server/router/router.js
+++ server/router/router.js
@@ -2,6 +2,7 @@
 export default function router (router, handle) {
     /** api **/
     router.get("/api/getWhiteListAuth/:address", require("../container/soga.js").getWhiteListAuth);
+    router.post("/api/whiteListMint", require("../container/soga.js").whiteListMint);
     
     // Default catch-all handler to allow Next.js to handle all other routes
     router.all("*", (req, res) => handle(req, res))

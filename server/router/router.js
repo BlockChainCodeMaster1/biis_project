204f9d43a90f4fbe8d8c8ca514993abe0238790a
aemoe <>
2024-01-09 19:10:14 +0800

    fixed: change api

--- server/router/router.js
+++ server/router/router.js
@@ -3,8 +3,8 @@ export default function router (router, handle) {
     /** api **/
     router.get("/api/getWhiteListAuth/:address", require("../container/soga.js").getWhiteListAuth);
     router.get("/api/getTotalData", require("../container/soga.js").getTotalData);
-    router.post("/api/whiteListMint", require("../container/soga.js").whiteListMint);
-    router.post("/api/publicMint", require("../container/soga.js").publicMint);
+    router.post("/api/whiteListMintNFT", require("../container/soga.js").whiteListMint);
+    router.post("/api/publicMintNFT", require("../container/soga.js").publicMint);
     router.post("/api/burnNft", require("../container/soga.js").burnNft);
 
     
